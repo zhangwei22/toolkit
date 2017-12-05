0aebdb73ce7a5694ed2a05b5e5c6b3af044fe58c
zhangwei <928241517@qq.com>
2017-12-05 19:58:02 +0800

    new a test file

---
+++ xiaomi-phone-buy/src/main/java/com/xiao/phone/buy/ForTest.java
@@ -0,0 +1,10 @@
+package com.xiao.phone.buy;
+
+/**
+ * Created by zhangwei on 2017/12/5.
+ */
+public class ForTest {
+    public static void main(String[] a ){
+        System.out.println("45");
+    }
+}
