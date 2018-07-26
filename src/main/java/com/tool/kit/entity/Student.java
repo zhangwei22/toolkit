9419fdc8b695f4df3416ff2c4a12b28438061d03
zhangwei <928241517@qq.com>
2018-07-26 14:04:38 +0800

    add a entity

---
+++ src/main/java/com/tool/kit/entity/Student.java
@@ -0,0 +1,26 @@
+package com.tool.kit.entity;
+
+/**
+ * Created by zhangwei on 2018/7/6.
+ */
+public class Student {
+    private String name;
+    private long idcard;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getIdcard() {
+        return idcard;
+    }
+
+    public void setIdcard(long idcard) {
+        this.idcard = idcard;
+    }
+}
+
